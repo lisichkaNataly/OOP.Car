46af5d8e30f45418200cd4d1042cbcff3c958b3a
Izmaylova <Natalyatf@yandex.ru>
2022-10-24 20:41:29 +0500

    ООП часть 1 Автомобили Домашнее задание 1 Домашнее задание 2 Домашнее задание 3

--- src/Car.java
+++ src/Car.java
@@ -1,27 +1,50 @@
 public class Car {
 
-    String brand;
-    String model;
-    double engineVolume;
-    String color;
-    int productionYear;
-    String productionCountry;
+    public String brand;
+    public String model;
+    public double engineVolume;
+    public String color;
+    public int productionYear;
+    public String productionCountry;
 
     Car(String brand, String model, double engineVolume, String color, int productionYear, String productionCountry) {
-        this.brand = brand;
-        this.model = model;
-        this.engineVolume = engineVolume;
-        this.color = color;
-        this.productionCountry = productionCountry;
-        this.productionYear = productionYear;
-    }
 
-    void car () {
-        System.out.println("Автомобиль: " + brand + " " + model);
-        System.out.println("Объем двигателя: " + engineVolume + " л");
-        System.out.println("Год выпуска: " + productionYear);
-        System.out.println("Страна выпуска: " + productionCountry);
-        System.out.println("Цвет: " + color);
+        if (brand == null) {
+            this.brand = "default";
+        } else {
+            this.brand = brand;
+        }
+
+        if (model == null) {
+            this.model = "default";
+        } else {
+            this.model = model;
+        }
+
+        if (productionCountry == null) {
+            this.productionCountry = "default";
+        } else {
+            this.productionCountry = productionCountry;
+        }
+
+        if (Double.compare(engineVolume, 0) == 0) {
+            this.engineVolume = 1.5;
+        } else {
+            this.engineVolume = engineVolume;
+        }
+
+        if (color == null) {
+            this.color = "белый";
+        } else {
+            this.color = color;
+        }
+
+        if (productionYear == 0) {
+            this.productionYear = 2000;
+        } else {
+            this.productionYear = productionYear;
+        }
+
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,83 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Автомобили");
-
-        Car lada = new Car("Lada", "Granta", 1.7, "желтый", 2015, "Россия");
-        lada.car();
-        System.out.println();
 
 
-        Car audi = new Car("Audi", "A8 50 L TDI quattro", 3.0, "черный", 2020, "Германия");
-        audi.car();
-        System.out.println();
+        Car lada = new Car("Lada",
+                "Granta",
+                1.7,
+                "желтый",
+                2015,
+                "Россия");
+        System.out.println("марка автомобиля: " + lada.brand +
+                " Модель: " + lada.model +
+                " Объем двигателя " + lada.engineVolume +
+                " Цвет кузова: " + lada.color +
+                " Год выпуска: " + lada.productionYear +
+                " Страна производства: " + lada.productionCountry);
 
-        Car bmv = new Car("BMV", "Z8", 3.0, "черный", 2020, "Германия");
-        bmv.car();
-        System.out.println();
 
-        Car kia = new Car("KIA", "Sportage 4-го поколения",2.4, "красный", 2018, "Южная Корея");
-        kia.car();
-        System.out.println();
+        Car audi = new Car("Audi",
+                "A8 50 L TDI quattro",
+                3.0,
+                null,
+                2020,
+                "Германия");
 
-        Car hyundai = new Car("Hyundai", "Avante", 1.6, "оранжевый", 2016, "Южная Корея");
-        hyundai.car();
-        System.out.println();
+        System.out.println("марка автомобиля: " + audi.brand +
+                " Модель: " + audi.model +
+                " Объем двигателя " + audi.engineVolume +
+                " Цвет кузова: " + audi.color +
+                " Год выпуска: " + audi.productionYear +
+                " Страна производства: " + audi.productionCountry);
+
+
+        Car bmv = new Car("BMV",
+                "Z8",
+                3.0,
+                "черный",
+                0,
+                "Германия");
+
+        System.out.println("марка автомобиля: " + bmv.brand +
+                " Модель: " + bmv.model +
+                " Объем двигателя " + bmv.engineVolume +
+                " Цвет кузова: " + bmv.color +
+                " Год выпуска: " + bmv.productionYear +
+                " Страна производства: " + bmv.productionCountry);
+
+
+
+        Car kia = new Car("KIA",
+                "Sportage 4-го поколения",
+                2.4,
+                "красный",
+                2018,
+                "Южная Корея");
+
+        System.out.println("Марка автомобиля: " + kia.brand +
+                " Модель " + kia.model +
+                " Объем двигателя " + kia.engineVolume +
+                " Цвет кузова: " + kia.color +
+                " Год выпуска:"  + kia.productionYear +
+                " Страна производства: " + kia.productionCountry);
+
+
+
+        Car hyundai = new Car("Hyundai",
+                "Avante",
+                0,
+                "оранжевый",
+                2016,
+                "Южная Корея");
+
+        System.out.println("Марка автомобиля " + hyundai.brand +
+                " Модель " + hyundai.model +
+                " Объем двигателя " + hyundai.engineVolume +
+                " Цвет кузова: " + hyundai.color +
+                " Год выпуска: " + hyundai.productionYear +
+                " Страна производства: " + hyundai.productionCountry);
+
+
 
 
 
